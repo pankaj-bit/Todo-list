a4df21b4ea61595b2a036fca7ecb6f3287c0f64d
pankaj-bit <55937021+pankaj-bit@users.noreply.github.com>
2021-01-23 12:08:53 +0530

    Updated Procfile

--- app.js
+++ app.js
@@ -137,11 +137,6 @@ app.get("/about", function(req, res){
 });
 
 
-let port=process.env.PORT;
-if(port==null || port==""){
-    port = 3000;
-}
-
-app.listen(port, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started on port 3000");
 });

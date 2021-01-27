2cccfe939f65d9eb4a70a87e1ccbcfc87b95c31f
pankajshendurkar <55937021+pankaj-bit@users.noreply.github.com>
2021-01-27 09:22:22 +0530

    updated app.js
    connect to local host

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true});
-mongoose.connect("mongodb+srv://<YOUR MONGODB SERVER LINK>/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true});
-
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true});
 
 const itemsScheme={
     name:String
